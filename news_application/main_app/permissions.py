e6cce59af9a027648aa1e03d98cb2aac640d01e4
Samia Islam <samia14islam@gmail.com>
2026-05-19 23:18:42 +0100

    Update permissions.py

--- news_application/main_app/permissions.py
+++ news_application/main_app/permissions.py
@@ -4,16 +4,31 @@ from rest_framework.permissions import BasePermission
 class IsJournalist(BasePermission):
     """Journalist permissions"""
     def has_permission(self, request, view):
-        return request.user.role == 'journalist'
+        return (
+            request.user.role == 'journalist'
+            or request.user.groups.filter(
+                name='Journalist'
+            ).exists()
+        )
 
 
 class IsEditor(BasePermission):
     """Editor permissions"""
     def has_permission(self, request, view):
-        return request.user.role == 'editor'
+        return (
+            request.user.role == 'editor'
+            or request.user.groups.filter(
+                name='Editor'
+            ).exists()
+        )
 
 
 class IsReader(BasePermission):
     """Reader permissions"""
     def has_permission(self, request, view):
-        return request.user.role == 'reader'
+        return (
+            request.user.role == 'reader'
+            or request.user.groups.filter(
+                name='Reader'
+            ).exists()
+        )
